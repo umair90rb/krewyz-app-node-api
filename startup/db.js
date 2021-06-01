d4b0fc7f8d4ba016c09a692ac975ae6fcfc49a37
Umair <umair90rb@gmail.com>
2021-06-01 16:43:22 +0500

    add real database

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,16 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
 
-module.exports = function () {
-  mongoose
-    .connect("mongodb://localhost:27017")
-    .then(() => winston.info("Connected to mongodb..."));
-};
-
 // module.exports = function () {
 //   mongoose
-//     .connect(
-//       "mongodb+srv://admin:onetofive@cluster0.zl6ds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-//     )
+//     .connect("mongodb://localhost:27017")
 //     .then(() => winston.info("Connected to mongodb..."));
 // };
+
+module.exports = function () {
+  mongoose
+    .connect(
+      "mongodb+srv://admin:onetofive@cluster0.zl6ds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    .then(() => winston.info("Connected to mongodb..."));
+};
